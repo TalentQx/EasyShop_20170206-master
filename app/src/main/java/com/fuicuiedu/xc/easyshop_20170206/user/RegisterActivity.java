5177c44e86bc13b56a5e6b91246ec8b135143586
TalentQx <635705365@qq.com>
2017-02-09 15:12:11 +0800

    Initial commit

---
+++ app/src/main/java/com/fuicuiedu/xc/easyshop_20170206/user/RegisterActivity.java
@@ -0,0 +1,114 @@
+package com.fuicuiedu.xc.easyshop_20170206.user;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.fuicuiedu.xc.easyshop_20170206.R;
+import com.fuicuiedu.xc.easyshop_20170206.commons.ActivityUtils;
+import com.fuicuiedu.xc.easyshop_20170206.commons.RegexUtils;
+import com.fuicuiedu.xc.easyshop_20170206.components.ProgressDialogFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.et_username)
+    EditText et_userName;
+    @BindView(R.id.et_pwd)
+    EditText et_pwd;
+    @BindView(R.id.et_pwdAgain)
+    EditText et_pwdAgain;
+    @BindView(R.id.btn_register)
+    Button btn_register;
+
+    private String username;
+    private String password;
+    private String pwd_again;
+    private ActivityUtils activityUtils;
+    private ProgressDialogFragment dialogFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        ButterKnife.bind(this);
+        activityUtils = new ActivityUtils(this);
+
+        init();
+    }
+
+    private void init() {
+        //给左上角加一个返回图标，需要重写菜单点击事件，否则点击无效
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        et_userName.addTextChangedListener(textWatcher);
+        et_pwd.addTextChangedListener(textWatcher);
+        et_pwdAgain.addTextChangedListener(textWatcher);
+    }
+
+    //给左上角加一个返回图标，需要重写菜单点击事件，否则点击无效
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //如果点击的是返回键，则finish
+        if (item.getItemId() == android.R.id.home) finish();
+        return super.onOptionsItemSelected(item);
+    }
+
+    private final TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            username = et_userName.getText().toString();
+            password = et_pwd.getText().toString();
+            pwd_again = et_pwdAgain.getText().toString();
+            boolean canLogin = !(TextUtils.isEmpty(username) || TextUtils.isEmpty(password) || TextUtils.isEmpty(pwd_again));
+            btn_register.setEnabled(canLogin);
+        }
+    };
+
+    @OnClick(R.id.btn_register)
+    public void onClick() {
+        if (RegexUtils.verifyUsername(username) != RegexUtils.VERIFY_SUCCESS) {
+            activityUtils.showToast(R.string.username_rules);
+            return;
+        } else if (RegexUtils.verifyPassword(password) != RegexUtils.VERIFY_SUCCESS) {
+            activityUtils.showToast(R.string.password_rules);
+            return;
+        } else if (!TextUtils.equals(password, pwd_again)) {
+            activityUtils.showToast(R.string.username_equal_pwd);
+            return;
+        }
+        activityUtils.showToast("执行注册的网络请求");
+//    网络请求：执行网络请求
+//    1。创建客户端。
+//            2.构建请求。
+//            2.1添加url（服务器地址，接口）
+//            2.2添加请求方式（get,post)
+//            2.3添加请求头（根据服务器的要求来添加，通常不需要）
+//            2.4添加请求体（可以为空）
+//            3.发送请求到服务器得到响应消息。
+//            4.解析响应消息
+//    4.1判断是否链接成功（判断响应码）
+//            4.2如果响应码是200-299  取出响应体（解析，展示）
+    }
+
+}
